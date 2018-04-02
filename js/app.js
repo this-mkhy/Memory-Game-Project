6ad22755240fcd7980c0047bafdba38bfb2a2d08
Mohamed Khaled Yousef <mkhy88519@gmail.com>
2018-04-02 11:52:40 +0200

    after import es6

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
+/*jslint es6:true, plusplus: true, evil:true */
+/*global console, alert, prompt*/
+ 
 //Declared variables and get element from HTML
-let card=document.getElementsByClassName("card");
-let cards=[...card]
+let card = document.getElementsByClassName("card");
+let cards = [...card]
 console.log(cards);
 
-var savedCards=[];
-const deck=document.getElementById("card-deck");
-let counts=document.querySelector(".moves");
-let matchedCard=document.getElementsByClassName("match");
-let starsList=document.querySelectorAll(".stars li");
-const stars=document.querySelectorAll(".fa-star");
+var savedCards = [];
+const deck = document.getElementById("card-deck");
+let counts = document.querySelector(".moves");
+let matchedCard = document.getElementsByClassName("match");
+let starsList = document.querySelectorAll(".stars li");
+const stars = document.querySelectorAll(".fa-star");
 
-let started,moves=0;
-
+let started,moves = 0;
 
 /*
  *  functionality
@@ -19,30 +21,30 @@ let started,moves=0;
 
 //to return shuffled cards
 function shuffle(array) {
-    var curr=array.length,temp,x;
-    while (curr!==0) 
+    var curr = array.length,temp,x;
+    while (curr !== 0) 
     {
         x=Math.floor(Math.random()*curr);
         curr-=1;
         temp=array[curr];
         
-        array[curr]=array[x];
-        array[x]=temp;
+        array[curr] = array[x];
+        array[x] = temp;
     }
 
     return array;
-};
+}
 
 document.body.onload=startGame();
 
 //when start new game 
 function startGame(){
-    started=false;
-    savedCards=[];
-    cards=shuffle(cards);
+    started = false;
+    savedCards = [];
+    cards = shuffle(cards);
     for (var i=0; i<cards.length ;++i)
     {
-        deck.innerHTML="";
+        deck.innerHTML = "";
         [].forEach.call(cards, function(item){
             deck.appendChild(item);
         });
@@ -50,25 +52,25 @@ function startGame(){
     }
     
     //reset moves and timer and rating
-    moves=0;
-    counts.innerHTML=moves;
-    for (var i=0; i<stars.length; ++i)
+    moves = 0;
+    counts.innerHTML = moves;
+    for (var j=0; j<stars.length ;++j)
     {
-        stars[i].style.color="#858ECE";
-        stars[i].style.visibility="visible";
+        stars[j].style.color = "#858ECE";
+        stars[j].style.visibility = "visible";
     }
-    second=0;
-    minute=0; 
-    hour=0;
+    second = 0;
+    minute = 0; 
+    hour = 0;
     
-    var timer=document.querySelector(".timer");
-    timer.innerHTML="0 mins 0 secs";
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
     clearInterval(interval);
 }
 
 
 //toggle functionality
-var displayCard=function (){
+var displayCard = function (){
     this.classList.toggle("open");
     this.classList.toggle("appear");
     this.classList.toggle("disabled");
@@ -78,18 +80,18 @@ var displayCard=function (){
 function cardOpen() {
     if (!started) 
     { 
-        second=0,minute=0,hour=0;
+        second = 0,minute=0,hour=0;
         startTimer(); 
-        started=true; 
+        started = true; 
     }
     
     //push the current card in the savedCards array then check if the sum=2
     savedCards.push(this);
-    var sum=savedCards.length;
-    if(sum===2)
+    var sum = savedCards.length;
+    if(sum === 2)
     {
         moveCounter();
-        if(savedCards[0].type===savedCards[1].type)
+        if(savedCards[0].type === savedCards[1].type)
         {
             matched();
         } 
@@ -109,7 +111,7 @@ function matched()
     savedCards[1].classList.add("match", "disabled");
     savedCards[0].classList.remove("appear", "open", "no-event");
     savedCards[1].classList.remove("appear", "open", "no-event");
-    savedCards=[];
+    savedCards = [];
 }
 
 
@@ -124,7 +126,7 @@ function unmatched(){
         savedCards[0].classList.remove("appear", "open", "no-event","unmatched");
         savedCards[1].classList.remove("appear", "open", "no-event","unmatched");
         enable();
-        savedCards=[];
+        savedCards = [];
     },1100);
 }
 
@@ -151,7 +153,7 @@ function enable(){
 //moveCounter function: to count moves and rates moves
 function moveCounter(){
     moves++;
-    counts.innerHTML=moves;
+    counts.innerHTML = moves;
     
     //rates
     if (moves>15 && moves<20)
@@ -164,7 +166,7 @@ function moveCounter(){
             }
         }
     }
-    else if(moves>=21)
+    else if(moves >= 21)
     {
         for(i=0; i<3 ;++i)
         {
@@ -178,43 +180,43 @@ function moveCounter(){
 
 
 //setup time
-var second=0,minute=0,hour=0;
-var timer=document.querySelector(".timer");
+var second = 0,minute = 0,hour = 0;
+var timer = document.querySelector(".timer");
 var interval;
 function startTimer(){
-    interval=setInterval(function(){
-        timer.innerHTML=minute+" mins and "+second+"secs";
+    interval = setInterval(function(){
+        timer.innerHTML = minute+" mins and "+second+"secs";
         second++;
-        if(second==60)
+        if(second == 60)
         {
             minute++;
-            second=0;
+            second = 0;
         }
-        if(minute==60)
+        if(minute == 60)
         {
             hour++;
-            minute=0;
+            minute = 0;
         }
     },1000);
 }
 
 //congratulations form to show message and final result of moves,time and rating
-let congratsMessage=document.getElementById("congrats")
+let congratsMessage = document.getElementById("congrats")
 function congratulationsForm(){
-    if(matchedCard.length==16)
+    if(matchedCard.length == 16)
     {
         clearInterval(interval);
-        finalTime=timer.innerHTML;
+        finalTime = timer.innerHTML;
 
         congratsMessage.classList.add("appear");
-        var starsRating=document.querySelector(".stars").innerHTML;
+        var starsRating = document.querySelector(".stars").innerHTML;
 
-        document.getElementById("end-move").innerHTML=moves;
-        document.getElementById("stars-rating").innerHTML=starsRating;
-        document.getElementById("all-time").innerHTML=finalTime;
+        document.getElementById("end-move").innerHTML = moves;
+        document.getElementById("stars-rating").innerHTML = starsRating;
+        document.getElementById("all-time").innerHTML = finalTime;
 
         closeIcon();
-    };
+    }
 }
 
 
@@ -237,7 +239,7 @@ function repeatGame(){
 //to add event listeners
 for (var i=0; i<cards.length ;++i)
 {
-    card=cards[i];
+    card = cards[i];
     card.addEventListener("click", displayCard);
     card.addEventListener("click", cardOpen);
     card.addEventListener("click",congratulationsForm);
